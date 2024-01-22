05ba892cd99551275d6145dcaa157e81df78155e
hjertus <theon@hjertuslot.com>
2024-01-22 23:12:36 +0100

    x

--- out-of-stock-products-last/out-of-stock-products-last/out-of-stock-products-last.php
+++ out-of-stock-products-last/out-of-stock-products-last/out-of-stock-products-last.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * The plugin bootstrap file
  *
@@ -15,7 +16,7 @@
  * @wordpress-plugin
  * Plugin Name:       Out Of Stock Products Last
  * Plugin URI:        https://hjertus.online
- * Description:       Puts all Out Of Stock Products Last in archive pages. 
+ * Description:       Puts all Out Of Stock Products Last in archive pages.
  * Version:           1.0.0
  * Author:            Theon
  * Author URI:        https://hjertus.online/
@@ -25,43 +26,47 @@
  * Domain Path:       /languages
  */
 
+add_filter( 'woocommerce_product_query_meta_query', 'filter_product_query_meta_query', 10, 2);
+function filter_product_query_meta_query ( $meta_query, $query) {
+    // On woocommerce home page only
+    if ( is_front_page() ){
+        // Exclude product "out of stock"
+        $meta_query[] = array(
+            'key' => '_stock_status',
+            'value' => 'outofstock',
+            'compare' => '!=',
+        );
+    }
+    return $meta_query;
+}
 
-/**
- * snippet       Sort Products By Stock Status - WooCommerce Shop
- * how-to        Get CustomizeWoo.com FREE
- * author        Rodolfo Melogli
- * compatible    WooCommerce 3.9
- * donate $9     https://businessbloomer.com/bloomer-armada/
- */
+class iWC_Orderby_Stock_Status {
 
-/**
- * Order product collections by stock status, instock products first.
- */
-class iWC_Orderby_Stock_Status
-{
-
-    public function __construct()
-    {
+    public function __construct() {
         // Check if WooCommerce is active
-        if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-            add_filter('posts_clauses', array($this, 'order_by_stock_status'), 2000);
+        if (is_plugin_active('woocommerce/woocommerce.php')) {
+            add_action('pre_get_posts', array($this, 'custom_orderby_stock_status'));
         }
     }
 
-    public function order_by_stock_status($posts_clauses)
-    {
-        global $wpdb;
-        // only change query on WooCommerce loops
-        if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag())) {
-            $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
-            $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
-            $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
+    public function custom_orderby_stock_status($query) {
+        if ($query->is_main_query() && (is_shop() || is_product_category() || is_product_tag())) {
+            $query->set('meta_key', '_stock_status');
+            $query->set('orderby', array('meta_value' => 'ASC', 'title' => 'ASC'));
+            $query->set('order', 'ASC');
+            $query->set('meta_query', array(
+                array(
+                    'key' => '_stock_status',
+                    'value' => '',
+                    'compare' => '!=',
+                ),
+            ));
         }
-        return $posts_clauses;
     }
 }
 
-new iWC_Orderby_Stock_Status;
+new iWC_Orderby_Stock_Status();
+
 
 // remove OOS products from related products in WooCommerce, because they are OOS! by Robin Scott of silicondales.com - see more at https://silicondales.com/tutorials/woocommerce/remove-out-of-stock-products-from-woocommerce-related-products/
 add_filter('woocommerce_related_products', 'exclude_oos_related_products', 10, 3);
